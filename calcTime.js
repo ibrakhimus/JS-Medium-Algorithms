a0824b50572a48434634485799f5ed5290eca161
Ibrakhim Ustelbay <randizzys@gmail.com>
2022-10-05 21:13:03 -0400

    calcTime

---
+++ calcTime.js
@@ -0,0 +1,12 @@
+function calcTime(seconds) {
+    let timerMinutes = Math.floor(seconds / 60);
+    let timerSeconds = seconds % 60;
+
+    if (timerMinutes.toString().length === 1) {
+        timerMinutes = "0" + timerMinutes
+    }
+
+    return timerMinutes + ':' + timerSeconds;
+}
+
+console.log(calcTime(200));
